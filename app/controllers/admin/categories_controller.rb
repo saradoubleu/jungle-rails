410e89903b33e14055ff80ec8ee7ce31285bea29
saradoubleu <your@email.com>
2018-06-21 22:40:01 +0000

    fully functioning add category flow

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -66,7 +66,7 @@ class Admin::CategoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def admin_category_params
-      params.require(:categories).permit(
+      params.require(:category).permit(
         :name
       )
     end
